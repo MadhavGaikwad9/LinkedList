0536228ba1a48df5bc73b0f8f55c005cdc7601f5
MadhavGaikwad9 <madhavgaikwad98476@gmail.com>
2026-03-31 21:51:50 +0530

    add in middle

--- LinkedList.java
+++ LinkedList.java
@@ -50,6 +50,25 @@ public class LinkedList {
         }
         System.out.println("null");
      }
+
+     //add in middle
+     public void addMiddle(int idx, int data){
+        if( idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i< idx-1){
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+     }
     public static void main(String args[]){
        LinkedList ll = new LinkedList();
         ll.print();
@@ -60,6 +79,8 @@ public class LinkedList {
         ll.addLast(3);
         ll.print();
         ll.addLast(4);
+        ll.addMiddle(2, 9);
+        
         ll.print();
 
     }
